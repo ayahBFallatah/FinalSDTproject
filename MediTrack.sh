0194d2879f1b9a33ca68baa76e35d0c127df3fdd
ayahBFallatah <ayah.b.fallatah@gmail.com>
2024-05-24 17:56:24 +0300

    2st shell commit

--- MediTrack.sh
+++ MediTrack.sh
@@ -19,3 +19,31 @@ find_in_file() {
     local search_term=$2
     grep -i "$search_term" "$file_name"
 }
+
+# Function to add a new patient
+add_patient() {
+    echo "Add New Patient"
+    echo "----------------"
+    read -p "Enter patient name: " name
+    read -p "Enter patient age: " age
+    read -p "Enter patient gender: " gender
+    data="$name, $age, $gender"
+    save_to_file "patients.txt" "$data"
+    echo "Patient added successfully."
+}
+
+# Function to view patient information
+view_patient_info() {
+    echo "View Patient Information"
+    echo "------------------------"
+    read_from_file "patients.txt"
+}
+
+# Function to search for a patient by name
+search_patient() {
+    echo "Search for Patient"
+    echo "------------------"
+    read -p "Enter patient name to search: " search_name
+    find_in_file "patients.txt" "$search_name"
+}
+
